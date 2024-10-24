8150bed077007534eddb134044acef0f70f98a0a
Professor833 <lalit.vd.dev@gmail.com>
2024-10-25 01:33:17 +0530

    feat: added status filter functionality

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -4,31 +4,65 @@ import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
+import { Status } from "@prisma/client";
 
 interface SearchParams {
   page: string;
+  status: string;
+}
+// Type guard to check if a string is a valid Status
+function isValidStatus(status: string): status is Status {
+  return Object.values(Status).includes(status as Status);
 }
 
 const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const pageSize = 10;
   const page = parseInt(searchParams.page) || 1;
-  const ticketCount = await prisma.ticket.count();
+
+  // const statuses = Object.values(Status);
+
+  // Use the type guard to safely check the status
+  const status = isValidStatus(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let where = {};
+  if (status) {
+    where = {
+      status
+    };
+  } else {
+    where = {
+      // if not status is received, return all tickets except closed
+      NOT: [{ status: "CLOSED" as Status }]
+    };
+  }
+  const ticketCount = await prisma.ticket.count({ where });
 
   const tickets = await prisma.ticket.findMany({
+    where,
     take: pageSize,
     skip: (page - 1) * pageSize
   });
 
   return (
     <div>
-      <Link
-        href={"/tickets/new"}
-        className={buttonVariants({ variant: "default" })}
-      >
-        New Ticket
-      </Link>
+      <div className="flex gap-2">
+        <Link
+          href={"/tickets/new"}
+          className={buttonVariants({ variant: "default" })}
+        >
+          New Ticket
+        </Link>
+        <StatusFilter />
+      </div>
       <DataTable tickets={tickets} />
-      <Pagination intemCount={26} pageSize={pageSize} currentPage={page} />
+      <Pagination
+        intemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
